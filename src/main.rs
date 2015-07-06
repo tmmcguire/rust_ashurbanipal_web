2cc528ffef966444b5eb449b718c79dce3299fab
Tommy M. McGuire <mcguire@crsr.net>
2015-07-05 19:25:29 -0500

    add method to Recommendation trait to sort result by score.

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,10 @@ fn main() {
     if args.len() < 3 { panic!("Usage: ashurbanipal_web pos-data topic-data"); }
     
     let style = style::Style::read(&args[1]);
-    let mut style_rec = style.scored_results(773).unwrap();
-    style_rec.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+    let style_rec = style.sorted_results(773).unwrap();
 
     let topic = topic::Topic::read(&args[2]);
-    let mut topic_rec = topic.scored_results(773).unwrap();
-    topic_rec.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+    let topic_rec = topic.sorted_results(773).unwrap();
 
     for &(etext,dist) in topic_rec.iter() {
         println!("{} {}", etext, dist);
--- src/recommendation.rs
+++ src/recommendation.rs
@@ -7,4 +7,16 @@ pub trait Recommendation {
     /// Return a vector of (etext number, score) pairs if possible.
     /// The vector will be sorted by etext_number.
     fn scored_results(&self, etext_no : Etext) -> Option<Vec<(Etext,f64)>>;
+
+    /// Return a vector of (etext number, score) pairs if possible,
+    /// sorted by score.
+    fn sorted_results(&self, etext_no : Etext) -> Option<Vec<(Etext,f64)>> {
+        match self.scored_results(etext_no) {
+            None => None,
+            Some(mut results) => {
+                results.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+                Some(results)
+            }
+        }
+    }
 }
