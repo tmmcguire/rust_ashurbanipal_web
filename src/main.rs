3d671602fc58ff95fee7be4eb00b3a4035263962
Tommy M. McGuire <mcguire@crsr.net>
2015-07-05 19:17:02 -0500

    test style and topic recommendations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod mbitset;
+mod recommendation;
+mod style;
+mod topic;
+
+use std::env;
+
+use recommendation::Recommendation;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    if args.len() < 3 { panic!("Usage: ashurbanipal_web pos-data topic-data"); }
+    
+    let style = style::Style::read(&args[1]);
+    let mut style_rec = style.scored_results(773).unwrap();
+    style_rec.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+
+    let topic = topic::Topic::read(&args[2]);
+    let mut topic_rec = topic.scored_results(773).unwrap();
+    topic_rec.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+
+    for &(etext,dist) in topic_rec.iter() {
+        println!("{} {}", etext, dist);
+    }
 }
