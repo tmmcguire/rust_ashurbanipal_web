dac681e4ab5604b8528294c392d099ab14703fca
Tommy M. McGuire <mcguire@crsr.net>
2015-07-07 17:53:50 -0500

    reimplement sorted_results to avoid unpacking the Option

--- src/recommendation.rs
+++ src/recommendation.rs
@@ -3,7 +3,7 @@
 /// An etext number.
 pub type Etext = usize;
 
-pub trait Recommendation {
+pub trait Recommendation : Sync {
     /// Return a vector of (etext number, score) pairs if possible.
     /// The vector will be sorted by etext_number.
     fn scored_results(&self, etext_no : Etext) -> Option<Vec<(Etext,f64)>>;
@@ -11,12 +11,9 @@ pub trait Recommendation {
     /// Return a vector of (etext number, score) pairs if possible,
     /// sorted by score.
     fn sorted_results(&self, etext_no : Etext) -> Option<Vec<(Etext,f64)>> {
-        match self.scored_results(etext_no) {
-            None => None,
-            Some(mut results) => {
-                results.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
-                Some(results)
-            }
-        }
+        self.scored_results(etext_no).map( |mut results| {
+            results.sort_by( |&(_,l),&(_,r)| l.partial_cmp(&r).unwrap() );
+            results
+        })
     }
 }
